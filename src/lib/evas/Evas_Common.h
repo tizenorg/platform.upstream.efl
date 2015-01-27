1c24dae24652e8ea755b2f5ff36dd67a45461579
Youngbok Shin <youngb.shin@samsung.com>
2015-01-27 17:16:26 +0900

    evas/edje: Add evas_textblock_cursor_cluster_* internal APIs.
    This is TIZEN_ONLY feature.
    
    Change-Id: Iced4c588c032485f1cbdcd0279904fe835872ed7

--- src/lib/evas/Evas_Common.h
+++ src/lib/evas/Evas_Common.h
@@ -3609,6 +3609,28 @@ EAPI Eina_Bool                                evas_textblock_cursor_char_next(Ev
 EAPI Eina_Bool                                evas_textblock_cursor_char_prev(Evas_Textblock_Cursor *cur) EINA_ARG_NONNULL(1);
 
 /**
+ * @internal
+ * TIZEN_ONLY(20150127): Add evas_textblock_cursor_cluster_* APIs.
+ * Advances 1 cluster forward.
+ * Cluster is an text ligature. 
+ *
+ * @param cur the cursor to advance.
+ * @return @c EINA_TRUE on success @c EINA_FALSE otherwise.
+ */
+EAPI Eina_Bool                                evas_textblock_cursor_cluster_next(Evas_Textblock_Cursor *cur) EINA_ARG_NONNULL(1);
+
+/**
+ * @internal
+ * TIZEN_ONLY(20150127): Add evas_textblock_cursor_cluster_* APIs.
+ * Advances 1 cluster backward.
+ * Cluster is an text ligature. 
+ *
+ * @param cur the cursor to advance.
+ * @return @c EINA_TRUE on success @c EINA_FALSE otherwise.
+ */
+EAPI Eina_Bool                                evas_textblock_cursor_cluster_prev(Evas_Textblock_Cursor *cur) EINA_ARG_NONNULL(1);
+
+/**
  * Moves the cursor to the start of the word under the cursor.
  *
  * @param cur the cursor to move.
@@ -3936,6 +3958,18 @@ EAPI int                                      evas_textblock_cursor_line_geometr
  * @return @c EINA_TRUE on success, @c EINA_FALSE otherwise.
  */
 EAPI Eina_Bool                                evas_textblock_cursor_char_coord_set(Evas_Textblock_Cursor *cur, Evas_Coord x, Evas_Coord y) EINA_ARG_NONNULL(1);
+
+/**
+ * @internal
+ * TIZEN_ONLY(20150127): Add evas_textblock_cursor_cluster_* APIs.
+ * Set the position of the cursor according to the X and Y coordinates and text ligature.
+ *
+ * @param cur the cursor to set.
+ * @param x coord to set by.
+ * @param y coord to set by.
+ * @return @c EINA_TRUE on success, @c EINA_FALSE otherwise.
+ */
+EAPI Eina_Bool                                evas_textblock_cursor_cluster_coord_set(Evas_Textblock_Cursor *cur, Evas_Coord x, Evas_Coord y) EINA_ARG_NONNULL(1);
 
 /**
  * Set the cursor position according to the y coord.

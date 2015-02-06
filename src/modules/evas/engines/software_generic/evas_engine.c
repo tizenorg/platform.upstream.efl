77a88107175dccccc4da35b1e47a77e362293ee6
Subhransu Sekhar Mohanty <sub.mohanty@samsung.com>
2015-02-06 12:01:11 +0900

    evas/engine : Fixed incorrect conversion from floating point by flushing mmx pipeline after draw.
    Conflicts:
    
    	src/modules/evas/engines/software_generic/evas_engine.c
    
    Change-Id: I4b55a738415371c2c9d1b6a3c67de1f50bd0c3e2

--- src/modules/evas/engines/software_generic/evas_engine.c
+++ src/modules/evas/engines/software_generic/evas_engine.c
@@ -545,7 +545,7 @@ _draw_thread_rectangle_draw(void *data)
     evas_common_rectangle_rgba_draw(rect->surface,
                                     rect->color, rect->render_op,
                                     rect->x, rect->y, rect->w, rect->h);
-
+    evas_common_cpu_end_opt();
     eina_mempool_free(_mp_command_rect, rect);
 }
 
@@ -3037,6 +3037,8 @@ _draw_thread_ector_draw(void *data)
                              ector->y,
                              ector->mul_col));
 
+   evas_common_cpu_end_opt();
+
    _draw_thread_ector_cleanup(ector);
 }
 
@@ -3066,7 +3068,6 @@ eng_ector_renderer_draw(void *data EINA_UNUSED, void *context, void *surface, Ec
         clip.w = dst->cache_entry.w;
         clip.h = dst->cache_entry.h;
      }
-
    //Hermet: Multiple Clippers???
    if (clips)
      {

8d17f66e53f46952a9565fc44ea51da0f2ba091e
Daekwang Ryu <dkdk.ryu@samsung.com>
2015-03-03 17:16:06 +0900

    Evas GL: Try to find the best matching config for GLES1
    merged from http://slp-info.sec.samsung.net/gerrit/#/c/698388/
    
    Change-Id: I4d0ec8f50c68bc39477f6245cd6ccb256a6d5b17
    Signed-off-by: Daekwang Ryu <dkdk.ryu@samsung.com>

--- src/modules/evas/engines/gl_common/evas_gl_core.c
+++ src/modules/evas/engines/gl_common/evas_gl_core.c
@@ -1501,7 +1501,7 @@ evgl_surface_create(void *eng_data, Evas_GL_Config *cfg, int w, int h)
 {
    EVGL_Surface *sfc = NULL;
    char *s = NULL;
-   int direct_override = 0, direct_mem_opt = 0;
+   int direct_override = 0, direct_mem_opt = 0, evgl_msaa = 0;
    Eina_Bool need_reconfigure = EINA_FALSE;
 
    // Check if engine is valid
@@ -1586,7 +1586,8 @@ evgl_surface_create(void *eng_data, Evas_GL_Config *cfg, int w, int h)
           }
 
         INF("Creating special surface for GLES 1.x rendering");
-        evgl_engine->funcs->gles1_surface_create(eng_data, sfc, cfg, w, h);
+        evgl_msaa = evgl_engine->caps.msaa_samples[(int) cfg->multisample_bits - 1];
+        evgl_engine->funcs->gles1_surface_create(eng_data, sfc, cfg, w, h, evgl_msaa);
      }
 
    // Create internal buffers

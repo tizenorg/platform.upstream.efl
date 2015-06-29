70f1bb72b64e1c54bc2cf18d0c0c5f1dfb0a57f6
DaeKwang Ryu <dkdk.ryu@samsung.com>
2015-06-29 15:51:26 +0900

    EvasGL: remove internal tags of doxygen
    Change-Id: I0ed7c9b77759e07188172bdd841c89c1baa718b7

--- src/lib/evas/Evas_GL_GLES2_Helpers.h
+++ src/lib/evas/Evas_GL_GLES2_Helpers.h
@@ -1,5 +1,4 @@
 /**
- * @internal
  * @file Evas_GL_GLES2_Helpers.h
  * @defgroup Evas_GL_GLES2_Helpers Evas GL GLES2 helpers
  * @ingroup Evas_GL
@@ -44,7 +43,6 @@ if (glExtensionFunction)
 // local convenience macros
 
 /**
- * @internal
  * @addtogroup Evas_GL_GLES2_Helpers
  * @{
  */

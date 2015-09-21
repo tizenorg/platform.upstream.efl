8024822792df264c44ddeaa05ec107e5b20eb177
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2015-09-21 16:07:36 +0300

    edje: edje_edit - add getters/setters for container align
    This functions designed that make the edje edit API's more
    homogeneous. The all edje attribute setters and getters
    treated only ony attribute, besides container API's.
    
    Change-Id: I53691251154daa1fd4df6f0c62007f1af7592c96
    origin: upstream

--- src/lib/edje/edje_edit.c
+++ src/lib/edje/edje_edit.c
@@ -4298,31 +4298,147 @@ FUNC_CONTAINER_BOOL(min, h)
 
 #undef FUNC_CONTAINER_BOOL
 
+#define FUNC_CONTAINER_INT(CLASS, VALUE) \
+EAPI int \
+edje_edit_state_container_##CLASS##_##VALUE##_get(Evas_Object *obj, const char *part, const char *state, double value) \
+{ \
+   int val; \
+   GET_PD_OR_RETURN(EINA_FALSE) \
+   switch (rp->part->type) \
+     { \
+      case EDJE_PART_TYPE_TABLE: \
+      { \
+         Edje_Part_Description_Table *table; \
+         table = (Edje_Part_Description_Table *)pd; \
+         val = table->table.CLASS.VALUE; \
+         break; \
+      } \
+      case EDJE_PART_TYPE_BOX: \
+      { \
+         Edje_Part_Description_Box *box; \
+         box = (Edje_Part_Description_Box *)pd; \
+         val = box->box.CLASS.VALUE; \
+         break; \
+      } \
+      default: \
+        val = 0; \
+     } \
+   return val; \
+} \
+EAPI Eina_Bool \
+edje_edit_state_container_##CLASS##_##VALUE##_set(Evas_Object *obj, const char *part, const char *state, double value, int new_val) \
+{ \
+   GET_PD_OR_RETURN(EINA_FALSE) \
+   switch (rp->part->type) \
+     { \
+      case EDJE_PART_TYPE_TABLE: \
+      { \
+         Edje_Part_Description_Table *table; \
+         table = (Edje_Part_Description_Table *)pd; \
+         table->table.CLASS.VALUE = new_val; \
+         break; \
+      } \
+      case EDJE_PART_TYPE_BOX: \
+      { \
+         Edje_Part_Description_Box *box; \
+         box = (Edje_Part_Description_Box *)pd; \
+         box->box.CLASS.VALUE = new_val; \
+         break; \
+      } \
+      default: \
+        return EINA_FALSE; \
+     } \
+   return EINA_TRUE; \
+}
+
+FUNC_CONTAINER_INT(padding, x)
+FUNC_CONTAINER_INT(padding, y)
+
+#undef FUNC_CONTAINER_INT
+
+#define FUNC_CONTAINER_DOUBLE(CLASS, VALUE) \
+EAPI double \
+edje_edit_state_container_##CLASS##_##VALUE##_get(Evas_Object *obj, const char *part, const char *state, double value) \
+{ \
+   double val; \
+   GET_PD_OR_RETURN(0.0) \
+   switch (rp->part->type) \
+     { \
+      case EDJE_PART_TYPE_TABLE: \
+      { \
+         Edje_Part_Description_Table *table; \
+         table = (Edje_Part_Description_Table *)pd; \
+         val = FROM_DOUBLE(table->table.CLASS.VALUE); \
+         break; \
+      } \
+      case EDJE_PART_TYPE_BOX: \
+      { \
+         Edje_Part_Description_Box *box; \
+         box = (Edje_Part_Description_Box *)pd; \
+         val = FROM_DOUBLE(box->box.CLASS.VALUE); \
+         break; \
+      } \
+      default: \
+         val = 0.0; \
+     } \
+   return val; \
+} \
+EAPI Eina_Bool \
+edje_edit_state_container_##CLASS##_##VALUE##_set(Evas_Object *obj, const char *part, const char *state, double value, double new_val) \
+{ \
+   GET_PD_OR_RETURN(EINA_FALSE) \
+   switch (rp->part->type) \
+     { \
+      case EDJE_PART_TYPE_TABLE: \
+      { \
+         Edje_Part_Description_Table *table; \
+         table = (Edje_Part_Description_Table *)pd; \
+         table->table.CLASS.VALUE = TO_DOUBLE(new_val); \
+         break; \
+      } \
+      case EDJE_PART_TYPE_BOX: \
+      { \
+         Edje_Part_Description_Box *box; \
+         box = (Edje_Part_Description_Box *)pd; \
+         box->box.CLASS.VALUE = TO_DOUBLE(new_val); \
+         break; \
+      } \
+      default: \
+        return EINA_FALSE; \
+     } \
+   return EINA_TRUE; \
+}
+
+FUNC_CONTAINER_DOUBLE(align, x)
+FUNC_CONTAINER_DOUBLE(align, y)
+
+#undef FUNC_CONTAINER_DOUBLE
+
 EAPI Eina_Bool
-edje_edit_state_container_align_get(Evas_Object *obj, const char *part,
-                                 const char *state, double value,
-                                 double *x, double *y)
+edje_edit_state_container_align_get(Evas_Object *obj, const char *part, const char *state, double value, double *x, double *y)
 {
    GET_PD_OR_RETURN(EINA_FALSE)
 
    switch (rp->part->type)
      {
       case EDJE_PART_TYPE_TABLE:
-        {
-           Edje_Part_Description_Table *table;
-           table = (Edje_Part_Description_Table*) pd;
-           if (x) *x = FROM_DOUBLE(table->table.align.x);
-           if (y) *y = FROM_DOUBLE(table->table.align.y);
-           break;
-        }
+      {
+         Edje_Part_Description_Table *table;
+         table = (Edje_Part_Description_Table *)pd;
+         if (x) *x = FROM_DOUBLE(table->table.align.x);
+         if (y) *y = FROM_DOUBLE(table->table.align.y);
+         break;
+      }
+
       case EDJE_PART_TYPE_BOX:
-        {
-           Edje_Part_Description_Box *box;
-           box = (Edje_Part_Description_Box*) pd;
-           if (x) *x = FROM_DOUBLE(box->box.align.x);
-           if (y) *y = FROM_DOUBLE(box->box.align.y);
-           break;
-        }
+      {
+         Edje_Part_Description_Box *box;
+         box = (Edje_Part_Description_Box *)pd;
+         if (x) *x = FROM_DOUBLE(box->box.align.x);
+         if (y) *y = FROM_DOUBLE(box->box.align.y);
+         break;
+      }
+
       default:
         return EINA_FALSE;
      }
@@ -4330,37 +4446,37 @@ edje_edit_state_container_align_get(Evas_Object *obj, const char *part,
 }
 
 EAPI Eina_Bool
-edje_edit_state_container_align_set(Evas_Object *obj, const char *part,
-                                    const char *state, double value,
-                                    double x, double y)
+edje_edit_state_container_align_set(Evas_Object *obj, const char *part, const char *state, double value, double x, double y)
 {
    GET_PD_OR_RETURN(EINA_FALSE)
 
    switch (rp->part->type)
      {
       case EDJE_PART_TYPE_TABLE:
-        {
-           Edje_Part_Description_Table *table;
-           table = (Edje_Part_Description_Table*) pd;
-           table->table.align.x = TO_DOUBLE(x);
-           table->table.align.y = TO_DOUBLE(y);
-           break;
-        }
+      {
+         Edje_Part_Description_Table *table;
+         table = (Edje_Part_Description_Table *)pd;
+         table->table.align.x = TO_DOUBLE(x);
+         table->table.align.y = TO_DOUBLE(y);
+         break;
+      }
+
       case EDJE_PART_TYPE_BOX:
-        {
-           Edje_Part_Description_Box *box;
-           box = (Edje_Part_Description_Box*) pd;
-           box->box.align.x = x;
-           box->box.align.y = y;
-           break;
-        }
+      {
+         Edje_Part_Description_Box *box;
+         box = (Edje_Part_Description_Box *)pd;
+         box->box.align.x = x;
+         box->box.align.y = y;
+         break;
+      }
+
       default:
         return EINA_FALSE;
      }
    return EINA_TRUE;
 }
 
-EAPI Eina_Bool 
+EAPI Eina_Bool
 edje_edit_state_container_padding_get(Evas_Object *obj, const char *part,
                                       const char *state, double value,
                                       int *x, int *y)

23fd13a9a50425acf7bbdbdb8bf7a7609d519a2e
Srivardhan Hebbar <sri.hebbar@samsung.com>
2015-10-21 14:04:43 -0700

    eina: add API for converting strbuf to lowercase.
    ==============================================================
       eina: add API for converting strbuf to lowercase.
    
       Summary:
       This API would be used in efl_network_websocket.
    
       Signed-off-by: Srivardhan Hebbar <sri.hebbar@samsung.com>
    
       Reviewers: cedric
    
       Differential Revision: https://phab.enlightenment.org/D3200
    
       Signed-off-by: Cedric BAIL <cedric@osg.samsung.com>
    ==============================================================
    
    Change-Id: I18506fe6756a00775b7c510266296a240aad665a
    Signed-Off-By: Jee-Yong Um <jc9.um@samsung.com>

--- src/lib/eina/eina_strbuf.h
+++ src/lib/eina/eina_strbuf.h
@@ -642,6 +642,17 @@ EAPI void eina_strbuf_ltrim(Eina_Strbuf *buf) EINA_ARG_NONNULL(1);
 EAPI void eina_strbuf_rtrim(Eina_Strbuf *buf) EINA_ARG_NONNULL(1);
 
 /**
+ * @internal
+ *
+ * @brief Convert the string to lower case.
+ *
+ * @param buf the string buffer to work with.
+ *
+ * This function converts all the characters in the strbuf to lower case.
+ */
+EAPI void eina_strbuf_tolower(Eina_Strbuf *buf) EINA_ARG_NONNULL(1);
+
+/**
  * @}
  */
 

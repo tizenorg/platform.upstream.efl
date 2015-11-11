72e3cc0c480dbdb7890d65d3c1c3bc5b768d8cf0
Gwanglim Lee <gl77.lee@samsung.com>
2015-11-11 18:35:31 +0900

    evas-wayland-egl: Fix crash when binding native surface, but evas_gl native surface still doesn't work well.
    Change-Id: I9b5997a76f5775219893d3cd391e3d84dff5be35
    
    Conflicts:
    	src/modules/evas/engines/wayland_egl/evas_engine.c

--- src/modules/evas/engines/wayland_egl/evas_engine.c
+++ src/modules/evas/engines/wayland_egl/evas_engine.c
@@ -1049,21 +1049,6 @@ _native_cb_bind(void *data EINA_UNUSED, void *image)
      {
         glBindTexture(GL_TEXTURE_2D, n->ns.data.opengl.texture_id);
      }
-  else if (n->ns.type == EVAS_NATIVE_SURFACE_EVASGL)
-    {
-        if (n->egl_surface)
-          {
-            void *surface = glsym_evgl_native_surface_buffer_get(n->egl_surface, NULL);
-            if (glsym_glEGLImageTargetTexture2DOES)
-              {
-                glsym_glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, surface);
-                if (eglGetError() != EGL_SUCCESS)
-                  ERR("glEGLImageTargetTexture2DOES() failed.");
-              }
-            else
-              ERR("Try glEGLImageTargetTexture2DOES on EGL with no support");
-          }
-    }
 }
 
 static void
@@ -1083,10 +1068,6 @@ _native_cb_unbind(void *data EINA_UNUSED, void *image)
      {
         glBindTexture(GL_TEXTURE_2D, 0);
      }
-  else if (n->ns.type == EVAS_NATIVE_SURFACE_EVASGL)
-    {
-      // nothing
-    }
 }
 
 static void

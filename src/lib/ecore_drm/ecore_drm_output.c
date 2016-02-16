df86cc7e216b8bb03f9f0e65b658d92e09a5fce4
Junkyeong Kim <jk0430.kim@samsung.com>
2016-02-16 18:22:50 +0900

    ecore_drm : add _ecore_drm_output_delete
    Change-Id: Ia0f52966ed2b57a2ee81176c31fa3abb783e7de5
    Signed-off-by: Junkyeong Kim <jk0430.kim@samsung.com>

--- src/lib/ecore_drm/ecore_drm_output.c
+++ src/lib/ecore_drm/ecore_drm_output.c
@@ -692,6 +692,36 @@ finish:
    _ecore_drm_output_frame_finish(output);
 }
 
+static void
+_ecore_drm_output_delete(Ecore_Drm_Device *dev, Ecore_Drm_Output *output)
+{
+   drmModeConnector *conn;
+   Ecore_Drm_Output_Mode *mode;
+   int i;
+   Eina_List *l;
+
+   if (!(conn = drmModeGetConnector(dev->drm.fd, output->conn_id)))
+     {
+        ERR("drmModeGetConnector fail\n");
+        return;
+     }
+
+   EINA_LIST_FREE(output->modes, mode)
+     free(mode);
+
+   if (output->dpms)
+     drmModeFreeProperty(output->dpms);
+   if (output->crtc)
+     drmModeFreeCrtc(output->crtc);
+   dev->crtc_allocator &= ~(1 << output->crtc_id);
+   dev->conn_allocator &= ~(1 << output->conn_id);
+   eina_stringshare_del(output->name);
+   eina_stringshare_del(output->model);
+   eina_stringshare_del(output->make);
+   free(output);
+   output = NULL;
+}
+
 void
 _ecore_drm_outputs_update(Ecore_Drm_Device *dev)
 {
@@ -699,6 +729,7 @@ _ecore_drm_outputs_update(Ecore_Drm_Device *dev)
    drmModeConnector *conn;
    int i = 0, x = 0, y = 0;
    Ecore_Drm_Output *output;
+   Ecore_Drm_Output *destory_output = NULL;
    uint32_t connected = 0, disconnects = 0;
 
    /* try to get drm resources */
@@ -758,7 +789,13 @@ next:
                {
                   disconnects &= ~(1 << output->conn_id);
                   _ecore_drm_output_event_send(output, EINA_FALSE);
+                  destory_output = output;
                }
+          }
+        if (destory_output)
+          {
+             eina_list_remove(dev->outputs, destory_output);
+             _ecore_drm_output_delete(dev, destory_output);
           }
      }
 }

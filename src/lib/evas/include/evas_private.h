bd55ae5b48bbf1a08909d6dc44585a7c8555e9f2
Youngbok Shin <youngb.shin@samsung.com>
2016-06-28 10:49:37 +0900

    evas: add pattern "style" when evas query fonts via fontconfig
    Summary:
    Some fonts can have weird style and weight value.
    If a font has a style name as "medium" and a weight value as "semi-bold",
    Evas can't load the font using "font=Somefont:style=Medium".
    It only can be load with "font=Somefont:style=SemiBold" or
    "font=Somefont:weight=SemiBold".
    On the other hand, it could be loaded when I tested the following commands.
    fc-match -s ":family=Somefont:style=Medium" or
    fc-match -s ":family=Somefont:weight=SemiBold"
    
    Evas also should load font based on font's style name.
    @fix
    
    Test Plan: N/A
    
    Reviewers: tasn, herdsman, cedric, woohyun, raster
    
    Reviewed By: raster
    
    Subscribers: Blackmole, z-wony, jpeg
    
    Differential Revision: https://phab.enlightenment.org/D4108
    
    Change-Id: Ibd9f53cb62568e2ee6e1e627f75607936396e6ff

--- src/lib/evas/include/evas_private.h
+++ src/lib/evas/include/evas_private.h
@@ -1162,6 +1162,7 @@ struct _Evas_Font_Description
    const char *name;
    const char *fallbacks;
    const char *lang;
+   const char *style;
 
    Evas_Font_Slant slant;
    Evas_Font_Weight weight;

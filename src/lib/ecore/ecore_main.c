1023e016221b3338afbd46f30c56a78fc225d9f6
Ivan Furs <i.furs@samsung.com>
2016-07-13 21:01:50 +0900

    EFL memory leak on Windows(handler continuously increasing)
    Summary:
    Fix: event need to clese  when create event:
      event = WSACreateEvent();
    
    Reviewers: bowonryu, herb, Jaehyun, thiepha, Hermet, jaehwan, cedric, raster
    
    Reviewed By: raster
    
    Subscribers: NikaWhite, reutskiy.v.v, artem.popov
    
    Differential Revision: https://phab.enlightenment.org/D4157
    
    Change-Id: I57324264520451330174905b57eb91e078a42e85

--- src/lib/ecore/ecore_main.c
+++ src/lib/ecore/ecore_main.c
@@ -2290,9 +2290,8 @@ _ecore_main_win32_select(int             nfds EINA_UNUSED,
 
    if (timeout == 0)
      {
-        free(objects);
-        free(sockets);
-        return 0;
+        res = 0;
+        goto err;
      }
 
    result = _ecore_main_win32_objects_wait(objects_nbr,
@@ -2388,6 +2387,7 @@ _ecore_main_win32_select(int             nfds EINA_UNUSED,
         res = -1;
      }
 
+err :
    /* Remove event objects again */
    for (i = 0; i < events_nbr; i++) WSACloseEvent(objects[i]);
 

176e5543240876aad371e2c4d7d6b906315a56e3
Joogab Yun <joogab.yun@samsung.com>
2016-08-02 10:32:21 +0900

    [evas_sw_backend] disable tbm buffer                   not ready yet.
    Change-Id: I7a4adaaa88c52aee2954fc9f06e76bd85b5480bb

--- src/modules/evas/engines/wayland_shm/evas_outbuf.c
+++ src/modules/evas/engines/wayland_shm/evas_outbuf.c
@@ -12,6 +12,12 @@
 Eina_Bool
 _evas_surface_init(Surface *s, int w, int h, int num_buf)
 {
+   if (getenv("EVAS_WAYLAND_USE_TBMBUF"))
+      if (_evas_tbmbuf_surface_create(s, w, h, num_buf)) return EINA_TRUE; 
+
+   if (_evas_shm_surface_create(s, w, h, num_buf)) return EINA_TRUE;
+
+/*
    if (getenv("EVAS_WAYLAND_USE_SHMBUF"))
       {
          if (_evas_shm_surface_create(s, w, h, num_buf)) return EINA_TRUE;
@@ -21,6 +27,7 @@ _evas_surface_init(Surface *s, int w, int h, int num_buf)
          if (_evas_tbmbuf_surface_create(s, w, h, num_buf)) return EINA_TRUE;
          if (_evas_shm_surface_create(s, w, h, num_buf)) return EINA_TRUE;
       }
+*/
 
    return EINA_FALSE;
 }
